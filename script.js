981d82338cbddd70dc8130468cce4d8fbf7b22f1
erick02out <erick.coragem@escola.pr.gov.br>
2025-04-03 01:16:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+   
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
+
     const aumentaFonteBotao document.getElementById('aumentar-fonte');
     const diminuiFonteBotao document.getElementById('diminuir-fonte');
 
-
+const alternaContraste = document.getElementById('alterna-contraste')
 
 let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -13,4 +21,9 @@ let tamanhoAtualFonte = 1;
             tamanhoAtualFonte -= 0.1;
             document.body.style.fontSize = `${tamanhoAtualFonte}rem`
  }
+})
+
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
 })
